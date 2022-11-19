0d12b9e329975487a4d9a7c0ed93a84d1be3312f
Cleuber Amaro <105400223+CleuberAmaro@users.noreply.github.com>
2022-11-19 16:34:58 -0300

    Initial Commit

---
+++ AppNotifica2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppNotifica2022
+//
+//  Created by Cleuber Amaro on 19/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
